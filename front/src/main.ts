3b6c49cd903625c054869a699e15ec7606c9e317
Antoine lin <lin.antoine.pro@gmail.com>
2022-12-03 15:09:07 +0100

    install vuetify

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,6 +1,20 @@
 import { createApp } from 'vue';
-import './style.css';
 import App from './App.vue';
+import './style.css';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+//vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(
+    router,
+    vuetify
+).mount('#app');
