f04d90f91097b81fab00f673af4fda1858bad839
Antoine lin <lin.antoine.pro@gmail.com>
2022-12-16 01:05:38 +0100

    add redirect and change view to component

---
+++ front/src/views/Session.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex h-full w-full">
+       <router-view />
+    </div>
+</template>
+
+<script lang="ts">
+
+import {defineComponent, onMounted} from 'vue';
+import {token} from "../service";
+
+export default defineComponent({
+    name: "Session",
+    beforeRouteEnter (to, from, next) {
+        const token = localStorage.getItem('token')
+
+        if (token) {
+            next({name: 'home'})
+        } else {
+            next()
+        }
+    },
+});
+</script>
